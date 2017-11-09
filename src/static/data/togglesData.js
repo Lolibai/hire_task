6566242a8f2498b8d5be9e7fae9e5a7b8a63785e
Mykhailo Shevchenko <mikleshef@gmail.com>
2017-11-09 18:51:29 +0200

    Multi option(large) fixed

--- src/static/data/togglesData.js
+++ src/static/data/togglesData.js
@@ -18,59 +18,59 @@ export const toggles = {
             right: 'Not active',
             mustbe: false
         },
-        {
-            id: 3,
-            left: 'Hot',
-            right: 'Cold',
-            mustbe: false
-        },
-        {
-            id: 4,
-            left: 'Good pay',
-            right: 'Bad pay',
-            mustbe: true
-        },
-        {
-            id: 5,
-            left: 'Active',
-            right: 'Not active',
-            mustbe: false
-        },        {
-            id: 6,
-            left: 'Hot',
-            right: 'Cold',
-            mustbe: false
-        },
-        {
-            id: 7,
-            left: 'Good pay',
-            right: 'Bad pay',
-            mustbe: true
-        },
-        {
-            id: 8,
-            left: 'Active',
-            right: 'Not active',
-            mustbe: false
-        },
-        {
-            id: 9,
-            left: 'Hot',
-            right: 'Cold',
-            mustbe: false
-        },
-        {
-            id: 10,
-            left: 'Good pay',
-            right: 'Bad pay',
-            mustbe: true
-        },
-        {
-            id: 11,
-            left: 'Active',
-            right: 'Not active',
-            mustbe: false
-        }
+        // {
+        //     id: 3,
+        //     left: 'Hot',
+        //     right: 'Cold',
+        //     mustbe: false
+        // },
+        // {
+        //     id: 4,
+        //     left: 'Good pay',
+        //     right: 'Bad pay',
+        //     mustbe: true
+        // },
+        // {
+        //     id: 5,
+        //     left: 'Active',
+        //     right: 'Not active',
+        //     mustbe: false
+        // },        {
+        //     id: 6,
+        //     left: 'Hot',
+        //     right: 'Cold',
+        //     mustbe: false
+        // },
+        // {
+        //     id: 7,
+        //     left: 'Good pay',
+        //     right: 'Bad pay',
+        //     mustbe: true
+        // },
+        // {
+        //     id: 8,
+        //     left: 'Active',
+        //     right: 'Not active',
+        //     mustbe: false
+        // },
+        // {
+        //     id: 9,
+        //     left: 'Hot',
+        //     right: 'Cold',
+        //     mustbe: false
+        // },
+        // {
+        //     id: 10,
+        //     left: 'Good pay',
+        //     right: 'Bad pay',
+        //     mustbe: true
+        // },
+        // {
+        //     id: 11,
+        //     left: 'Active',
+        //     right: 'Not active',
+        //     mustbe: false
+        // }
     ],
     question: "What are the ideal conditions inside an office?"
 };
